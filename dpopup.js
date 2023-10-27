3e2297ef8e51fca92288774d37c0b728b7bb0bfc
우재하 <jaeha6049@naver.com>
2023-10-27 17:48:26 +0900

    자바스크립트 수정

--- dpopup.js
+++ dpopup.js
@@ -1,5 +1,40 @@
 function sayHello(){
-    document.body.innerText = "Hello, World!";
+    document.body.style.backgroundColor = "yellow";
 }
+
+const element = $("body");
+
+const shown = true;
+
+
+
+setInterval(toggle, 500);
+
+
+ 
+
+
+
+function toggle() {
+// 깜빡거림
+   if(shown) {
+
+       element.hide();
+
+       shown = false;
+
+   } else {
+
+       element.show();
+
+       shown = true;
+
+   }
+
+}
+
+
+
+
 // 페이지가 완전히 로딩된 후 함수 실행
 window.onload = sayHello;
