d6852ee2d3317c167306491af11ec2ad79153037
우재하 <jaeha6049@naver.com>
2023-10-31 14:14:59 +0900

    1.0.2 update

--- dpopup.js
+++ dpopup.js
@@ -1,40 +1,8 @@
-function sayHello(){
-    document.body.style.backgroundColor = "yellow";
+const mute = (user) => {
+    
 }
-
-const element = $("body");
-
-const shown = true;
-
-
-
-setInterval(toggle, 500);
-
-
- 
-
-
-
-function toggle() {
-// 깜빡거림
-   if(shown) {
-
-       element.hide();
-
-       shown = false;
-
-   } else {
-
-       element.show();
-
-       shown = true;
-
-   }
-
-}
-
 
 
 
 // 페이지가 완전히 로딩된 후 함수 실행
-window.onload = sayHello;
+// window.onload = sayHello;
--- my-content-script.js
+++ my-content-script.js
@@ -1,10 +1,43 @@
-const sendMessageToCurrentPage = (name, message) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (pages) => {
-      chrome.tabs.sendMessage(pages[0].id, { name, message });
-    });
-  };
+function save() { chrome.storage.local.get(["onoff"]).then((result) => {
+
+  if (result.onoff == "ON"){
+
+
   
-  setInterval(() => {
-    sendMessageToCurrentPage("firstMessage", "Hi Content Script!");
-  }, 10000);
-  
+    chrome.storage.local.get(["mute"]).then((result) => {
+
+mute = result.mutelist.split(';')
+
+let last = mutelist.pop()
+
+console.log(mute)
+
+
+/* 종목토론방 => 개발자도구에서 태그명 또는 id, class 찾아서
+   쿼리셀렉터 인자 바꿀것 */
+let authors = document.querySelectorAll(".p11")
+for (let i = 0; i < authors.length; i++) {
+data = authors[i].innerHTML
+
+const item = authors.item(i);
+
+for (let i = 0; i < mute.length; i++){
+
+if (data.includes(mute[j])){
+  item.parentNode.parentNode.removeChild(item.parentNode)
+
+
+}
+}
+}
+
+
+    }
+    );
+  }
+});
+
+
+}
+save();
+
